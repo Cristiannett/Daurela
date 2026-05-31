238430e21f8ff50322468563a8a815a7b0ce4e7a
Cristiannett <98850933+Cristiannett@users.noreply.github.com>
2026-05-31 18:17:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daurela-202605311614';
+const CACHE = 'daurela-202605311616';
 const ASSETS = ['/Daurela/'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
